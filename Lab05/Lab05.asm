33ad66fc5fb23747d4fd723f57e6bed49c8dee23
Neslisah <neslisah.celek@outlook.com>
2022-12-26 14:29:54 +0300

    Create Lab05.asm

---
+++ Lab05/Lab05.asm
@@ -0,0 +1,53 @@
+org 100h
+#start=Emulation_Kit.exe#
+
+mov di, 0
+
+mainloop:
+    mov dx, 2000h
+    add dx, di
+    
+    mov bx, 0
+    
+print:
+    mov si, 0
+    mov cx, 5
+    
+    cmp dx, 2027h 
+    jle next      
+    MOV CX, 0FFH
+    WAIT:
+    LOOP WAIT
+    mov dx, 2000h ;son bosluga geldiyse
+    
+next:
+    mov al, dots[bx][si]
+    out dx, al 
+    inc si
+    inc dx       
+    
+    cmp si, 5
+    loopne next
+    
+    add bx, 5
+    cmp bx, 40
+    jl print  
+    
+    add di, 5
+    
+loop mainloop
+    
+    
+    
+
+
+ret
+dots DB	01111111b, 00000100b, 00001000b, 00010000b, 01111111b  ; N
+	 DB	01111111b, 01001001b, 01001001b, 01001001b, 01000001b  ; E	 
+	 DB	01000010b, 01001001b, 01001001b, 01001001b, 00110110b  ; B
+	 DB	00111110b, 01000001b, 01000001b, 01000001b, 00100010b  ; C
+	 DB	01111111b, 01000001b, 01000001b, 00100010b, 00011100b  ; D
+	
+	 DB	01111111b, 00001001b, 00001001b, 00001001b, 00000001b  ; F
+	 DB	00111110b, 01000001b, 01001001b, 01001001b, 01111010b  ; G
+	 DB	01111111b, 00001000b, 00001000b, 00001000b, 01111111b  ; H
